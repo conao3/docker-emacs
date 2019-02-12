525368372045bfd7e21bdf7a5290b32c4e23508a
conao3 <conao3@gmail.com>
2019-02-13 05:02:59 +0900

    import clostache

--- helper/project.clj
+++ helper/project.clj
@@ -3,7 +3,8 @@
   :url "http://github.com/conao3/docer-emacs"
   :license {:name "AGPLv3"
             :url "https://www.gnu.org/licenses/agpl-3.0.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [de.ubercode.clostache/clostache "1.4.0"]]
   :main ^:skip-aot helper.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- helper/src/helper/core.clj
+++ helper/src/helper/core.clj
@@ -1,7 +1,9 @@
 (ns helper.core
-  (:gen-class))
+  (:gen-class)
+  (:use [clostache.parser]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (println (render "Hello, {{name}}!" {:name "Felix"})))
