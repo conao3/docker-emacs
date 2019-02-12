8405ba2db6ac67f5aa015e65a79a9aaf7c5969d4
conao3 <conao3@gmail.com>
2019-02-13 04:48:46 +0900

    generate clojure helper files

---
+++ helper/project.clj
@@ -0,0 +1,9 @@
+(defproject helper "0.1.0-SNAPSHOT"
+  :description "docker-emacs helper"
+  :url "http://github.com/conao3/docer-emacs"
+  :license {:name "AGPLv3"
+            :url "https://www.gnu.org/licenses/agpl-3.0.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :main ^:skip-aot helper.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ helper/test/helper/core_test.clj
@@ -0,0 +1,7 @@
+(ns helper.core-test
+  (:require [clojure.test :refer :all]
+            [helper.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
