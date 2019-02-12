4cf723098dad49caebc349327cbd3d26b4b6cb2d
conao3 <conao3@gmail.com>
2019-02-13 06:32:17 +0900

    remove unneed comment

--- helper/src/helper/core.clj
+++ helper/src/helper/core.clj
@@ -6,11 +6,7 @@
 
 (def cli-options
   ;; An option with a required argument
-  [;; ["-p" "--port PORT" "Port number"
-   ;;  :default 80
-   ;;  :parse-fn #(Integer/parseInt %)
-   ;;  :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
-   ;; ;; A non-idempotent option (:default is applied first)
+  [
    ;; ["-v" nil "Verbosity level"
    ;;  :id :verbosity
    ;;  :default 0
