e47506168b36b6862954e3efbe8b1d950c2b2b2f
conao3 <conao3@gmail.com>
2019-02-13 05:16:45 +0900

    add generate-dockerfiles function

--- helper/src/helper/core.clj
+++ helper/src/helper/core.clj
@@ -2,8 +2,15 @@
   (:gen-class)
   (:use [clostache.parser]))
 
+(defn- generate-dockerfiles
+  "generate dockerfiles"
+  [^String version]
+  (render "Hello, {{name}}!" {:name version}))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!")
-  (println (render "Hello, {{name}}!" {:name "Felix"})))
+  (println (render "Hello, {{name}}!" {:name "Felix"}))
+  (println (generate-dockerfiles "26.1")))
+
