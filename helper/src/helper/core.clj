25a15e27f4feb310e992864b687b3d902cd46c01
conao3 <conao3@gmail.com>
2019-02-13 06:14:46 +0900

    commentout unuse options

--- helper/src/helper/core.clj
+++ helper/src/helper/core.clj
@@ -6,16 +6,16 @@
 
 (def cli-options
   ;; An option with a required argument
-  [["-p" "--port PORT" "Port number"
-    :default 80
-    :parse-fn #(Integer/parseInt %)
-    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
-   ;; A non-idempotent option (:default is applied first)
-   ["-v" nil "Verbosity level"
-    :id :verbosity
-    :default 0
-    :update-fn inc] ; Prior to 0.4.1, you would have to use:
-   ;; :assoc-fn (fn [m k _] (update-in m [k] inc))
+  [;; ["-p" "--port PORT" "Port number"
+   ;;  :default 80
+   ;;  :parse-fn #(Integer/parseInt %)
+   ;;  :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
+   ;; ;; A non-idempotent option (:default is applied first)
+   ;; ["-v" nil "Verbosity level"
+   ;;  :id :verbosity
+   ;;  :default 0
+   ;;  :update-fn inc] ; Prior to 0.4.1, you would have to use:
+   ;; ;; :assoc-fn (fn [m k _] (update-in m [k] inc))
    ;; A boolean option defaulting to nil
    ["-h" "--help"]])
 
